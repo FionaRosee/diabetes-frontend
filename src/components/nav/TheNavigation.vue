dc753d6379fb28fff44d90511c0ac52b50b6759d
dengemann90 <72465696+dengemann90@users.noreply.github.com>
2022-06-17 18:29:42 +0200

    layout navigation added

---
+++ src/components/nav/TheNavigation.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="nav container-fluid my-5 d-flex justify-content-center">
+    <div class="row">
+      <div class="col justify-content-end">
+        <a class="button-filled m-2 rounded-pill" href="#" role="button"></a>
+        <a class="button-filled m-2 rounded-pill" href="#" role="button"></a>
+        <a class="button-filled m-2 rounded-pill" href="#" role="button"></a>
+        <a class="button-filled m-2 rounded-pill" href="#" role="button"></a>
+        <a class="button-unfilled m-2 rounded-pill" href="#" role="button"></a>
+        <a class="button-unfilled m-2 rounded-pill" href="#" role="button"></a>
+        <a class="button-unfilled m-2 rounded-pill" href="#" role="button"></a>
+        <a class="button-unfilled m-2 rounded-pill" href="#" role="button"></a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.button-filled {
+  background-color: #4fb0e0ad;
+  padding: 0.05rem 2.5rem;
+}
+.button-filled:hover {
+  background-color: #4fb0e0;
+}
+
+.button-unfilled {
+  background-color: #c0c0c06d;
+  padding: 0.05rem 2.5rem;
+}
+
+.button-unfilled:hover {
+  background-color: #c0c0c0;
+}
+</style>
