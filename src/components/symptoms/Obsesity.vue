33a322cbcd9b4c4ddee3da9cfaa6714c144321a2
dengemann90 <72465696+dengemann90@users.noreply.github.com>
2022-06-20 16:48:30 +0200

    validation input fields added

--- src/components/symptoms/Obsesity.vue
+++ src/components/symptoms/Obsesity.vue
@@ -19,8 +19,9 @@
               class="form-select"
               id="gender"
               aria-label="Floating label select example"
+              v-model = "gender"
+              placeholder=" "
             >
-              <option selected></option>
               <option value="m">Male</option>
               <option value="f">Female</option>
             </select>
@@ -28,35 +29,27 @@
           </div>
         </div>
 
-        <!-- age-group -->
-        <div class="col mt-2">
-          <div class="form-floating">
-            <select
-              class="form-select"
-              id="age-group"
-              aria-label="Floating label select example"
-            >
-              <option selected></option>
-              <option value="1">0 - 39</option>
-              <option value="2">40 - 59</option>
-              <option value="3">60 - 100</option>
-            </select>
-            <label for="age-group">Age</label>
-          </div>
-        </div>
+        <!-- age -->
+       <form class="form-floating mt-2" v-on:submit.prevent>
+          <input class="form-control" type="number" :class="{invalidInput : ageInputInvalid}" id="age" v-model="age"  placeholder=" " autocomplete="off" @blur="inputValidation(1,120,'age')"/>
+          <label for="age">Age</label>
+          <p v-if="ageInputInvalid" class="my-2 text-danger">Please enter a valid age between 1 and 120!</p>
+        </form>
 
         <!-- height -->
 
-        <form class="form-floating mt-2">
-          <input class="form-control" id="height" />
+        <form class="form-floating mt-2" v-on:submit.prevent>
+          <input class="form-control" type="number" :class="{invalidInput: heightInputInvalid}" id="height" v-model="height" placeholder=" " autocomplete="off" @blur="inputValidation(0.55,250,'height')"/>
+          <p v-if="heightInputInvalid" class="my-2 text-danger">Please enter a valid height between 0.55cm and 250cm!</p>
           <label for="height">Height (cm)</label>
         </form>
 
         <!-- weight -->
 
-        <form class="form-floating mt-2">
-          <input class="form-control" id="weight" />
-          <label for="weight">Wheigt (kg)</label>
+        <form class="form-floating mt-2" v-on:submit.prevent>
+          <input class="form-control" type="number" :class="{invalidInput: weightInputInvalid}" id="weight" v-model="weight" placeholder=" " autocomplete="off" @blur="inputValidation(10,250,'weight')"/>
+          <p v-if="weightInputInvalid" class="my-2 text-danger">Please enter a valid weight between 10kg and 250kg!</p>
+          <label for="weight">Weight (kg)</label>
         </form>
 
         <!-- end of form -->
@@ -97,6 +90,57 @@
   </div>
 </template>
 
+<script>
+export default {
+  data(){
+    return {
+      ageInputInvalid: false,
+      heightInputInvalid: false,
+      weightInputInvalid: false,
+      gender: '',
+      age: '',
+      height: '',
+      weight: ''
+    }
+  },
+  methods:{
+    inputValidation(min, max, id) {
+
+    if(id == 'age'){
+      if(this.age >= min && this.age <= max){
+        this.ageInputInvalid = false
+        console.log('ageInputInvalid :', this.ageInputInvalid)
+      } else{
+        this.ageInputInvalid = true
+        console.log('ageInputInvalid :', this.ageInputInvalid)
+      }
+    }
+
+    if(id == 'height'){
+      if(this.height >= min && this.height <= max){
+        this.heightInputInvalid = false
+        console.log('heightInputInvalid :', this.heightInputInvalid)
+      } else{
+        this.heightInputInvalid = true
+        console.log('heightInputInvalid :', this.heightInputInvalid)
+      }
+    }
+
+    if(id == 'weight'){
+      if(this.weight >= min && this.weight <= max){
+      this.weightInputInvalid = false
+      console.log('weightInputInvalid :', this.weightInputInvalid)
+    } else{
+      this.weightInputInvalid = true
+      console.log('weightInputInvalid :', this.weightInputInvalid)
+    }
+  }    
+
+    }
+  }
+}
+</script>
+
 <style scoped>
 
 .btn-outline-primary{
@@ -108,6 +152,23 @@
 .btn-outline-primary:hover{
     border-color: #4fb0e0;
     background-color: #4fb0e0;
+}
+
+.invalidInput{
+border-color: #dc3545;
+}
+
+/* hide arrow in input field for numbers */
+/* https://www.w3schools.com/howto/howto_css_hide_arrow_number.asp */
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+/* Firefox */
+input[type="number"] {
+  -moz-appearance: textfield;
 }
 
 </style>>
