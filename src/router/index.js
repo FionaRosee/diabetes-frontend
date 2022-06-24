9525bb73093c72523ab6626aa39afdbaecbe21ad
dengemann90 <72465696+dengemann90@users.noreply.github.com>
2022-06-24 10:23:09 +0200

    new view (symptom) added

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import QuestionPageOne from "../views/Question Page One.vue";
 import GeneralInformation from "../views/GeneralInformation.vue";
+import QuestionPageFourteen from "../views/QuestionPageFourteen.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +22,11 @@ const router = createRouter({
       name: "questionpageone",
       component: QuestionPageOne,
     },
+    {
+      path: "/question_page_fourteen",
+      name: "qp14",
+      component: QuestionPageFourteen,
+    }
   ],
 });
 
---
+++ src/views/QuestionPageFourteen.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="main-layout container-fluid">
+<the-navigation></the-navigation>
+<alopecia></alopecia>
+</div>
+</template>
+
+<script>
+import TheNavigation from '../components/nav/TheNavigation.vue'
+import Alopecia from '../components/symptoms/Alopecia.vue'
+export default{
+components:{
+  TheNavigation,
+  Alopecia
+}
+}
+</script>
+
+<style scoped>
+
+</style>
