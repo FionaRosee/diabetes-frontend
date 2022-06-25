2de7c5927b6ddd912a8e2e945f066c390a3ccbf9
dengemann90 <72465696+dengemann90@users.noreply.github.com>
2022-06-25 18:24:00 +0200

    component muscle stiffness added

--- src/views/QuestionPageThirteen.vue
+++ src/views/QuestionPageThirteen.vue
@@ -1,17 +1,17 @@
 <template>
 <div class="main-layout container-fluid">
 <the-navigation></the-navigation>
-
+<muscle-stiffness></muscle-stiffness>
 </div>
 </template>
 
 <script>
 import TheNavigation from '../components/nav/TheNavigation.vue'
-
+import MuscleStiffness from '../components/symptoms/MuscleStiffness.vue'
 export default{
 components:{
   TheNavigation,
-
+  MuscleStiffness
 }
 }
 </script>
