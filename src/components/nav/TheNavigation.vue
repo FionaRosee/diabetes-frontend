7840b9cc699620e8a79dd85ef4a90c1af9517bea
dengemann90 <72465696+dengemann90@users.noreply.github.com>
2022-06-27 17:22:27 +0200

    navigation bar validation + removed link title

--- src/components/nav/TheNavigation.vue
+++ src/components/nav/TheNavigation.vue
@@ -1,82 +1,66 @@
 <template>
-  <div class="nav container-fluid my-3 d-flex justify-content-center">
-    <div class="row">
-      <div class="col">
-        <p class="active" :class="{ inactive: linkTitle == 'empty' }">
-          {{ linkTitle }}
-        </p>
-      </div>
-    </div>
-  </div>
-  <div class="nav container-fluid my-3 d-flex justify-content-center">
+  <div class="nav container-fluid my-3 d-flex justify-content-center"></div>
+  <div class="nav container-fluid my-4 d-flex justify-content-center">
     <div class="row">
       <div class="col justify-content-end">
         <router-link
-          class="button-filled m-2 rounded-pill"
+          :class="mode('General information')"
+          class="m-2 rounded-pill"
           role="button"
           to="/general_information"
         ></router-link>
-        <a class="button-filled m-2 rounded-pill" href="#" role="button"></a>
-        <a class="button-filled m-2 rounded-pill" href="#" role="button"></a>
-        <a class="button-filled m-2 rounded-pill" href="#" role="button"></a>
-        <a class="button-unfilled m-2 rounded-pill" href="#" role="button"></a>
-        <a class="button-unfilled m-2 rounded-pill" href="#" role="button"></a>
+        <a class="buttonFilled m-2 rounded-pill" href="#" role="button"></a>
+        <a class="buttonFilled m-2 rounded-pill" href="#" role="button"></a>
+        <a class="buttonFilled m-2 rounded-pill" href="#" role="button"></a>
+        <a class="buttonUnfilled m-2 rounded-pill" href="#" role="button"></a>
+        <a class="buttonnfilled m-2 rounded-pill" href="#" role="button"></a>
         <router-link
-          class="button-unfilled m-2 rounded-pill"
+          :class="mode('Genital thrush')"
+          class="m-2 rounded-pill"
           role="button"
           to="/question_page_seven"
-          @mouseover="setLinkTitle('Genital thrush')"
-          @mouseleave="clearLinkTitle"
         ></router-link>
         <router-link
-          class="button-unfilled m-2 rounded-pill"
+          :class="mode('Visual blurring')"
+          class="m-2 rounded-pill"
           role="button"
           to="/question_page_eight"
-          @mouseover="setLinkTitle('Visual blurring')"
-          @mouseleave="clearLinkTitle"
         ></router-link>
         <router-link
-          class="button-unfilled m-2 rounded-pill"
+          :class="mode('Itching')"
+          class="m-2 rounded-pill"
           role="button"
           to="/question_page_nine"
-          @mouseover="setLinkTitle('Itching')"
-          @mouseleave="clearLinkTitle"
         ></router-link>
         <router-link
-          class="button-unfilled m-2 rounded-pill"
+          :class="mode('Irritability')"
+          class="m-2 rounded-pill"
           role="button"
           to="/question_page_ten"
-          @mouseover="setLinkTitle('Irritability')"
-          @mouseleave="clearLinkTitle"
         ></router-link>
         <router-link
-          class="button-unfilled m-2 rounded-pill"
+          :class="mode('Delayed healing')"
+          class="m-2 rounded-pill"
           role="button"
           to="/question_page_eleven"
-          @mouseover="setLinkTitle('Delayed healing')"
-          @mouseleave="clearLinkTitle"
         ></router-link>
         <router-link
-          class="button-unfilled m-2 rounded-pill"
+          :class="mode('Partial paresis')"
+          class="m-2 rounded-pill"
           role="button"
           to="/question_page_twelve"
-          @mouseover="setLinkTitle('Partial paresis')"
-          @mouseleave="clearLinkTitle"
         ></router-link>
         <router-link
-          class="button-unfilled m-2 rounded-pill"
+          :class="mode('Muscle stiffness')"
+          class="m-2 rounded-pill"
           role="button"
           to="/question_page_thirteen"
-          @mouseover="setLinkTitle('Muscle stiffness')"
-          @mouseleave="clearLinkTitle"
         ></router-link>
         <router-link
-          :class="{activePage: activePage == 'Circular hair loss'}"
-          class="button-unfilled m-2 rounded-pill"
+          :class="mode('Circular hair loss')"
+          class="m-2 rounded-pill"
           role="button"
           to="/question_page_fourteen"
-          @mouseover="setLinkTitle('Circular hair loss')"
-          @mouseleave="clearLinkTitle"
         ></router-link>
       </div>
     </div>
@@ -84,47 +68,156 @@
 </template>
 
 <script>
+import { get } from "idb-keyval";
 export default {
   data() {
     return {
-      linkTitle: "empty",
-      activePage: "",
+      dataBackendRequestDB: "",
     };
   },
   methods: {
-    setLinkTitle(title) {
-      this.linkTitle = title;
+    //dynamic button style
+    mode(linkId) {
+      //General informaiton
+      if (linkId == "General information") {
+        if (linkId == this.$route.name) {
+          return { activePage: true };
+        } else if (this.dataBackendRequestDB.Age == null  && this.dataBackendRequestDB.Gender == null && this.dataBackendRequestDB.Obesity == null) {
+          return { buttonUnfilled: true };
+        } else {
+          return { buttonFilled: true };
+        }
+      }
+
+      //Genital thrush
+      if (linkId == "Genital thrush") {
+        if (linkId == this.$route.name) {
+          return { activePage: true };
+        } else if (this.dataBackendRequestDB.Genital_thrush == null) {
+          return { buttonUnfilled: true };
+        } else {
+          return { buttonFilled: true };
+        }
+      }
+
+      //visual blurring
+      if (linkId == "Visual blurring") {
+        if (linkId == this.$route.name) {
+          return { activePage: true };
+        } else if (this.dataBackendRequestDB.visual_blurring == null) {
+          return { buttonUnfilled: true };
+        } else {
+          return { buttonFilled: true };
+        }
+      }
+
+      //Itching
+      if (linkId == "Itching") {
+        if (linkId == this.$route.name) {
+          return { activePage: true };
+        } else if (this.dataBackendRequestDB.Itching == null) {
+          return { buttonUnfilled: true };
+        } else {
+          return { buttonFilled: true };
+        }
+      }
+
+      //Irritability
+      if (linkId == "Irritability") {
+        if (linkId == this.$route.name) {
+          return { activePage: true };
+        } else if (this.dataBackendRequestDB.Irritability == null) {
+          return { buttonUnfilled: true };
+        } else {
+          return { buttonFilled: true };
+        }
+      }
+
+      //Delayed healing
+      if (linkId == "Delayed healing") {
+        if (linkId == this.$route.name) {
+          return { activePage: true };
+        } else if (this.dataBackendRequestDB.delayed_healing == null) {
+          return { buttonUnfilled: true };
+        } else {
+          return { buttonFilled: true };
+        }
+      }
+
+      //Partial paresis
+      if (linkId == "Partial paresis") {
+        if (linkId == this.$route.name) {
+          return { activePage: true };
+        } else if (this.dataBackendRequestDB.partial_paresis == null) {
+          return { buttonUnfilled: true };
+        } else {
+          return { buttonFilled: true };
+        }
+      }
+
+      //Muscle stiffness
+      if (linkId == "Muscle stiffness") {
+        if (linkId == this.$route.name) {
+          return { activePage: true };
+        } else if (this.dataBackendRequestDB.muscle_stiffness == null) {
+          return { buttonUnfilled: true };
+        } else {
+          return { buttonFilled: true };
+        }
+      }
+
+      //Circular hair loss
+      if (linkId == "Circular hair loss") {
+        if (linkId == this.$route.name) {
+          return { activePage: true };
+        } else if (this.dataBackendRequestDB.Alopecia == null) {
+          return { buttonUnfilled: true };
+        } else {
+          return { buttonFilled: true };
+        }
+      }
     },
-    clearLinkTitle() {
-      this.linkTitle = "empty";
-    }
+    loadDataDB() {
+      get("dataBackendRequest").then((data) => {
+        if (data != null) {
+          this.dataBackendRequestDB = data;
+          console.dir(data);
+        }
+      });
+    },
+  },
+  created() {
+    this.loadDataDB();
   },
 };
 </script>
 <style scoped>
-.button-filled {
+.buttonFilled {
   background-color: #4fb0e0ad;
   padding: 0.05rem 2rem;
 }
-.button-filled:hover {
+.buttonFilled:hover {
   background-color: #4fb0e0;
-    border: 1.5px solid #c0c0c06d;
 }
 
-.button-unfilled {
+.buttonUnfilled {
   background-color: #c0c0c06d;
   padding: 0.05rem 2rem;
 }
 
-.button-unfilled:hover {
+.buttonUnfilled:hover {
   background-color: #c0c0c0;
 }
 
-.active {
+.activeLinkTitle {
   color: #c0c0c0;
 }
-.inactive {
+.inactiveLinkTitle {
   color: white;
 }
 
+.activePage {
+  padding: 0.05rem 2rem;
+  background-color: #377d9dc6;
+}
 </style>
