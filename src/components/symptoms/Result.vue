5efb17b816444b8ef2f665648b14c1f84d543082
dengemann90 <72465696+dengemann90@users.noreply.github.com>
2022-06-28 21:20:21 +0200

    dummy post backend implemented -> successfull

--- src/components/symptoms/Result.vue
+++ src/components/symptoms/Result.vue
@@ -57,34 +57,35 @@ export default {
               "Content-Type": "application/json",
             },
             body: JSON.stringify({
-              // your expected POST request payload goes here
-              'Age': 45,
-              'Gender': 1,
-              'Polyuria': 0,
-              'Polydipsia': 0,
-              'sudden weight loss': 0,
-              'weakness': 1,
-              'Polyphagia': 0,
-              'Genital thrush': 0,
-              'visual blurring': 0,
-              'Itching': 0,
-              'Irritability': 0,
-              'delayed healing': 0,
-              'partial paresis': 0,
-              'muscle stiffness': 0,
-              'Alopecia': 0,
-              'Obesity': 0,
+              //POST request payload
+              Age: 35,
+              Gender: 0,
+              Polyuria: 0,
+              Polydipsia: 0,
+              "sudden weight loss": 1,
+              weakness: 1,
+              Polyphagia: 0,
+              "Genital thrush": 0,
+              "visual blurring": 0,
+              Itching: 0,
+              Irritability: 0,
+              "delayed healing": 0,
+              "partial paresis": 0,
+              "muscle stiffness": 0,
+              Alopecia: 0,
+              Obesity: 0,
             }),
           }
         );
         const data = await response.json();
-        // enter you logic when the fetch is successful
+        // request successfull
+        setTimeout(() => {
         this.showSpinner = false;
         this.loading = false;
         this.response = data;
         console.log(data);
+        }, 3000);
       } catch (error) {
-        // enter your logic for when there is an error (ex. error toast)
         console.log(error);
       }
     },
